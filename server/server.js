25842eee9c0aefbf669ef2d8d6aefa530e52d699
Vinu <133609222+vinu-codes@users.noreply.github.com>
2024-11-19 11:06:32 +1100

    authentication, set up .env

--- server/server.js
+++ server/server.js
@@ -1,15 +1,56 @@
 const express = require('express') // Import express
-
+const axios = require('axios') // Import axios
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express() // Make express app
+app.use(bodyParser.json())
+app.use(cors()) // Enable CORS for all origins
 
-app.post('/login', (req, res) => {})
-// Create a POST route for /login endpoint why?
-// POST submits new data to the server.
-// In this case, we are submitting login data to the server
-// req is the request object, res is the response object
-// request object contains the data sent by the client
-// response object is used to send a response back to the client
-// GET retrieves resources.
-// POST submits new data to the server.
-// PUT updates existing data.
-// DELETE removes data.
+require('dotenv').config()
+
+const CLIENT_ID = process.env.CLIENT_ID
+const REDIRECT_URI = 'http://localhost:3000'
+const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
+const CLIENT_SECRET = process.env.CLIENT_SECRET
+const RESPONSE_TYPE = 'token'
+
+app.post('/login', async (req, res) => {
+  const code = req.body.code
+  console.log('Request Body:', code)
+
+  if (!code) {
+    return res.status(400).json({ error: 'Authorization code required' })
+  }
+
+  try {
+    const response = await axios.post(
+      'https://accounts.spotify.com/api/token',
+      new URLSearchParams({
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: REDIRECT_URI,
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    )
+
+    console.log({ response: response }, 'hi')
+    // res.send(JSON.parse(response))
+  } catch (error) {
+    console.error('Error exchanging code for token:', error)
+    res.status(500).json({ error: 'Failed to exchange code for token' })
+  }
+})
+
+const PORT = '3001'
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
+
+// URL encoded string
+// grant_type=authorization_code&code=<your_code>&redirect_uri=<your_redirect_uri>&client_id=<your_client_id>&client_secret=<your_client_secret>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,34 +1,48 @@
 import React, { useEffect } from 'react'
+import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
-
-const CLIENT_ID = '3ef3db19f6a54b198dc8db937118237c'
+const CLIENT_ID = 'c731efae217b45818bc43ad7e1a14218'
 const REDIRECT_URI = 'http://localhost:3000'
 const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
-const RESPONSE_TYPE = 'token'
+const RESPONSE_TYPE = 'code'
 
 const AUTH_URL = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&response_type=${RESPONSE_TYPE}&redirect_uri=${REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
 
 const Login = () => {
-  const navigate = useNavigate() // Initialize the useNavigate hook
+  const navigate = useNavigate()
+  const codeStr = new URLSearchParams(window.location.search).get('code')
+  const code = {
+    code: codeStr,
+  }
+
   useEffect(() => {
-    console.log('hit login')
-    // get the hash of the url
-    const hash = window.location.hash
-
-    let token = window.localStorage.getItem('token')
-
-    if (!token && hash) {
-      // set token in local storage
-      // get the url, split by '&', get the first element, split by '=', get the second element
-      token = hash.split('&')[0].split('=')[1]
-      window.localStorage.setItem('token', token)
-      navigate('/dashboard')
+    if (!code) {
+      console.log('No code')
+      return
     }
-  }, [])
+
+    const loginCall = async () => {
+      const config = {
+        url: 'http://localhost:3001/login',
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        data: code,
+      }
+      try {
+        const response = await axios(config)
+        console.log(response.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    loginCall()
+  }, [code])
 
   return (
     <div>
-      <a href={AUTH_URL}>Login to Spotify</a>
+      <button>
+        <a href={AUTH_URL}>Login to Spotify</a>
+      </button>
     </div>
   )
 }
