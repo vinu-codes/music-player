da480c1a4cd2363cc30e40f8c55535442dac683c
MiracleBlue <MiracleBlue@users.noreply.github.com>
2024-11-20 09:27:10 +1100

    fixed redirect yeahhh

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSpotify, useSpotifyContext } from '@hooks/SpotifyProvider'
-import { redirect } from 'react-router-dom'
+import { redirect, replace, useNavigate } from 'react-router-dom'
 
 const CLIENT_ID = 'c731efae217b45818bc43ad7e1a14218'
 const REDIRECT_URI = 'http://localhost:3000'
@@ -10,14 +10,21 @@ const RESPONSE_TYPE = 'code'
 const AUTH_URL = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&response_type=${RESPONSE_TYPE}&redirect_uri=${REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
 
 const Login = () => {
+  const navigate = useNavigate()
   if (window.location.search.includes('code')) {
-    useSpotify(() => redirect('/dashboard'));
+
+    useSpotify(() => {
+      console.log('we best redirect')
+      navigate('/dashboard')
+    });
+
     return (
       <div>
         <h1>Authenticating...</h1>
       </div>
     )
   }
+
   return (
     <div>
       <button>
